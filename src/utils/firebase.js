f3d0b279b9f2775f25395b04c2da5e38d9c86947
vivek15292001 <vivek15292001@gmail.com>
2025-04-28 22:49:04 +0530

    deployed on firebase

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBK1lIiJNCBI5mCjtZGXH3eUx1fJEJZzBM",
+  authDomain: "netflix-eb5a6.firebaseapp.com",
+  projectId: "netflix-eb5a6",
+  storageBucket: "netflix-eb5a6.firebasestorage.app",
+  messagingSenderId: "519090286731",
+  appId: "1:519090286731:web:585bd05f5e263cdf72248e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
