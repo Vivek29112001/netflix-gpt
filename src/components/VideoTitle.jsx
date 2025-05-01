694f56a186d91d5a03980aa5cf508353c993a18b
vivek15292001 <vivek15292001@gmail.com>
2025-05-01 09:04:10 +0530

    trailer video working

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import InfoOutlineIcon from '@mui/icons-material/InfoOutline';
 
 const VideoTitle = ({ title, overview }) => {
   const [hovered, setHovered] = useState(false);
@@ -12,7 +10,7 @@ const VideoTitle = ({ title, overview }) => {
       onMouseLeave={() => setHovered(false)}
     >
       <h1
-        className={`text-5xl m-10  font-bold font-mono px-2 py-2 transition-transform duration-200 cursor-pointer w-1/3 ${
+        className={`text-5xl m-10 font-bold font-mono px-2 py-2 transition-transform duration-200 cursor-pointer w-1/3 ${
           hovered ? 'scale-120 text-6xl' : 'scale-120'
         }`}
       >
@@ -25,13 +23,11 @@ const VideoTitle = ({ title, overview }) => {
         </p>
       )}
       <div className="flex gap-4 mt-4">
-        <button className="flex items-center gap-2  bg-white text-black px-6 py-2 rounded font-semibold hover:bg-gray-300 transition duration-200 opacity-80">
-          <PlayArrowIcon className="text-black" />
-          Play
+        <button className="flex items-center gap-2 bg-white text-black px-6 py-2 rounded font-semibold hover:bg-gray-300 transition duration-200 opacity-80">
+          <span className="text-black">▶️</span> Play
         </button>
-        <button className="flex items-center gap-2  bg-gray-700 text-white px-6 py-2 rounded border border-gray-400 hover:bg-gray-600 transition duration-200 opacity-80 ">
-          <InfoOutlineIcon className="text-white" />
-          More Info
+        <button className="flex items-center gap-2 bg-gray-700 text-white px-6 py-2 rounded border border-gray-400 hover:bg-gray-600 transition duration-200 opacity-80">
+          <span className="text-black">ℹ️</span> More Info
         </button>
       </div>
     </div>
